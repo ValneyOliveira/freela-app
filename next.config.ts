2586f1757a317ebf69d24aacaa5830c6cd6aaa6b
valney std <valneyfelipe@hotmail.com>
2025-07-19 21:04:34 -0300

    feat: adicionar arquivo de config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: isProd,
+  },
 };
 
 export default nextConfig;
