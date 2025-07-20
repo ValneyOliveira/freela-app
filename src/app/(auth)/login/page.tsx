62622377332112641533669c804b31498d8c71d3
valney std <valneyfelipe@hotmail.com>
2025-07-19 22:59:34 -0300

    feat: remover arquivos svg não usados

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 const page = () => {
   return (
-
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-blue-50">
 
       <div className="w-full max-w-sm">
